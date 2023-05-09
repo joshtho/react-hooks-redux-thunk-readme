1bcb7286857194cac6dff37eab9c88243e49d384
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-05-09 18:41:47 -0500

    commit tree 4bc734bd2b0adde1275ece3e128e187048de181b

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 import rootReducer from "./reducer";
 
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composedEnhancer
 );
 
 ReactDOM.render(
